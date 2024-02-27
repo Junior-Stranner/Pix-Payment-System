2eb9403eeba81677292324a2bef34c69842aeccc
Junior-Stranner <Ju-stranner@outlook.com>
2024-02-27 18:35:03 -0300

    start the project

---
+++ payment-system/src/main/java/br/com/jujubaprojects/paymentsystem/PaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.jujubaprojects.paymentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentSystemApplication.class, args);
+	}
+
+}
