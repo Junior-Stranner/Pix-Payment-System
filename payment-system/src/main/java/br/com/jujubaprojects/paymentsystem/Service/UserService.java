3805981a12b899d3eabc080d9f8d2952743b1463
Junior-Stranner <Ju-stranner@outlook.com>
2024-02-28 18:44:18 -0300

    have a lot of problems with tokenService and SecurityFilter

--- payment-system/src/main/java/br/com/jujubaprojects/paymentsystem/Service/UserService.java
+++ payment-system/src/main/java/br/com/jujubaprojects/paymentsystem/Service/UserService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 import br.com.jujubaprojects.paymentsystem.Entity.User;
 import br.com.jujubaprojects.paymentsystem.Repository.UserRepository;
 import br.com.jujubaprojects.paymentsystem.Utils.RandomString;
-import br.com.jujubaprojects.paymentsystem.dto.UserResponseDTO;
 
 @Service
 public class UserService {
@@ -20,7 +19,7 @@ public class UserService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public UserResponseDTO registerUser(User user){
+    public User registerUser(User user){
         List<User> users = this.userRepository.findAll();
         boolean emailExists = users.stream().anyMatch(eExists -> eExists.getEmail().equals(user.getEmail()));
         if(userRepository.findByEmail(user.getEmail()) != null || emailExists){
@@ -32,15 +31,8 @@ public class UserService {
             String randomeCode = RandomString.generateRandomString(64);
             user.setVerificationCode(randomeCode);
             user.setEnable(false);
-             User savedUser = this.userRepository.save(user);
 
-             UserResponseDTO userResponse = new UserResponseDTO(
-                savedUser.getId(),
-                savedUser.getName(),
-                savedUser.getEmail(),
-                savedUser.getPassword());
-
-            return userResponse;
+            return  this.userRepository.save(user);
 
         }
         
